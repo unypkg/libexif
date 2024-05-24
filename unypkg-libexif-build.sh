fa4db5558a11abec9977cf3c272c44377b163ebb
michacassola <mich@casso.la>
2024-05-24 22:16:21 +0200

    Update unypkg-libexif-build.sh

--- unypkg-libexif-build.sh
+++ unypkg-libexif-build.sh
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="libexif"
-pkggit="https://github.com/libexif/libexif.git refs/tags/*"
+pkggit="https://github.com/libexif/libexif.git refs/tags/libexif-*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "libexif-[0-9_]+-release$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "libexif-[0-9.].*" | sed -e "s|libexif-||" -e "s|-release||" -e "s|_|.|g")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -77,11 +77,15 @@ get_include_paths
 
 unset LD_RUN_PATH
 
+autoreconf -i
+
 ./configure \
-    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
+    --prefix=/uny/pkg/"$pkgname"/"$pkgver" \
+    --disable-static \
+    --with-doc-dir=/uny/pkg/"$pkgname"/"$pkgver"/share/doc/libexif
 
 make -j"$(nproc)"
-make -j"$(nproc)" check 
+
 make -j"$(nproc)" install
 
 ####################################################
